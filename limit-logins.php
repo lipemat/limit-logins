46632f67c7892c5691454c2a12be803280ff26ee
lipemat <mat@matlipe.com>
2024-04-20 15:42:50 -0500

    Bump to 0.13.0

--- limit-logins.php
+++ limit-logins.php
@@ -8,7 +8,7 @@ namespace Lipe\Limit_Logins;
  * Description: Limit rate of login attempts and block IP or username temporarily.
  * Author: Mat Lipe
  * Author URI: https://onpointplugins.com
- * Version: 0.12.0
+ * Version: 0.13.0
  * Text Domain: lipe
  * License: MIT
  * Network: false
