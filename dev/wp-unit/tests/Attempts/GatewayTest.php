8fecd954f35144504538c1c84bc39e4fbd359beb
lipemat <mat@matlipe.com>
2024-04-20 11:20:30 -0500

    Support `rest_api` in possible login gateway failures
    If a REST request is currently being served, mark the gateway as `rest_api` in
    the log.

---
+++ dev/wp-unit/tests/Attempts/GatewayTest.php
@@ -0,0 +1,61 @@
+<?php
+declare( strict_types=1 );
+
+namespace Lipe\Limit_Logins\Attempts;
+
+use Lipe\Limit_Logins\Attempts;
+
+/**
+ * @author Mat Lipe
+ * @since  April 2024
+ *
+ */
+class GatewayTest extends \WP_UnitTestCase {
+	protected function tearDown(): void {
+		unset( $GLOBALS['wp_xmlrpc_server'] );
+		parent::tearDown();
+	}
+
+
+	public function test_detect_wp_login(): void {
+		$gateway = Gateway::detect();
+		$this->assertSame( Gateway::WP_LOGIN, $gateway );
+
+		$user = self::factory()->user->create_and_get();
+		wp_authenticate( $user->user_login, 'not valid password' );
+		$this->assertSame( Gateway::WP_LOGIN, Attempts::in()->get_existing( $user->user_login )->gateway );
+	}
+
+
+	public function test_detect_xmlrpc(): void {
+		$GLOBALS['wp_xmlrpc_server'] = new \wp_xmlrpc_server();
+		$gateway = Gateway::detect();
+		$this->assertSame( Gateway::XMLRPC, $gateway );
+
+		$user = self::factory()->user->create_and_get();
+		wp_authenticate( $user->user_login, 'not valid password' );
+		$this->assertSame( Gateway::XMLRPC, Attempts::in()->get_existing( $user->user_login )->gateway );
+	}
+
+
+	public function test_detect_woo_login(): void {
+		$_POST['woocommerce-login-nonce'] = 'nonce';
+		$gateway = Gateway::detect();
+		$this->assertSame( Gateway::WOO_LOGIN, $gateway );
+
+		$user = self::factory()->user->create_and_get();
+		wp_authenticate( $user->user_login, 'not valid password' );
+		$this->assertSame( Gateway::WOO_LOGIN, Attempts::in()->get_existing( $user->user_login )->gateway );
+	}
+
+
+	public function test_detect_rest_api(): void {
+		add_filter( 'wp_is_rest_endpoint', '__return_true' );
+		$gateway = Gateway::detect();
+		$this->assertSame( Gateway::REST_API, $gateway );
+
+		$user = self::factory()->user->create_and_get();
+		wp_authenticate( $user->user_login, 'not valid password' );
+		$this->assertSame( Gateway::REST_API, Attempts::in()->get_existing( $user->user_login )->gateway );
+	}
+}
--- src/Attempts/Gateway.php
+++ src/Attempts/Gateway.php
@@ -4,6 +4,7 @@ declare( strict_types=1 );
 namespace Lipe\Limit_Logins\Attempts;
 
 enum Gateway: string {
+	case REST_API = 'rest_api';
 	case WP_LOGIN = 'wp_login';
 	case WOO_LOGIN = 'woo_login';
 	case XMLRPC = 'xmlrpc';
@@ -12,8 +13,23 @@ enum Gateway: string {
 	public static function detect(): self {
 		if ( isset( $_POST['woocommerce-login-nonce'] ) ) { //phpcs:ignore WordPress.Security.NonceVerification
 			return self::WOO_LOGIN;
-		} elseif ( isset( $GLOBALS['wp_xmlrpc_server'] ) && is_object( $GLOBALS['wp_xmlrpc_server'] ) ) {
+		}
+
+		if ( \defined( 'XMLRPC_REQUEST' ) && XMLRPC_REQUEST ) {
 			return self::XMLRPC;
+		}
+
+		if ( isset( $GLOBALS['wp_xmlrpc_server'] ) && $GLOBALS['wp_xmlrpc_server'] instanceof \IXR_Server ) {
+			return self::XMLRPC;
+		}
+
+		if ( \function_exists( 'wp_is_rest_endpoint' ) && wp_is_rest_endpoint() ) {
+			return self::REST_API;
+		}
+
+		// @todo Remove after minimum WP version is 6.5+ as this will be covered by wp_is_rest_endpoint().
+		if ( \defined( 'REST_REQUEST' ) && REST_REQUEST ) {
+			return self::REST_API;
 		}
 
 		return self::WP_LOGIN;
