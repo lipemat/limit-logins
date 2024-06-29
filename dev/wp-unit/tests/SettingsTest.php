e7e339dfc22a1bc55de34cb6e6dce59a2133e51d
lipemat <mat@matlipe.com>
2024-06-29 15:08:38 -0400

    Require version 5.0.2 of wordpress-libs

--- dev/wp-unit/tests/SettingsTest.php
+++ dev/wp-unit/tests/SettingsTest.php
@@ -20,7 +20,7 @@ class SettingsTest extends \WP_UnitTestCase {
 			$this->assertNotFalse( get_option( $option['option_name'] ) );
 		}
 		$global_count = $wpdb->get_var( 'SELECT COUNT(*) from ' . $wpdb->options );
-		$this->assertSame( Type::CHECKBOX, call_private_method( Repo::in(), 'get_field', [ Settings::CLEAR ] )->get_type() );
+		$this->assertSame( Type::CHECKBOX, call_private_method( Repo::in(), 'get_registered', [ Settings::CLEAR ] )->get_type() );
 
 		$find_query = $wpdb->prepare( "SELECT COUNT(*) FROM {$wpdb->options} WHERE option_name LIKE %s", 'limit_login_%' );
 		$this->assertSame( 24, (int) $wpdb->get_var( $find_query ) );
@@ -35,7 +35,7 @@ class SettingsTest extends \WP_UnitTestCase {
 		}
 
 		do_action( 'cmb2_init' );
-		$this->assertSame( Type::TITLE, call_private_method( Repo::in(), 'get_field', [ Settings::CLEAR ] )->get_type() );
+		$this->assertSame( Type::TITLE, call_private_method( Repo::in(), 'get_registered', [ Settings::CLEAR ] )->get_type() );
 	}
 
 
